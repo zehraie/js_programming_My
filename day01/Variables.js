9a5a5f2923b201f18bf5c96a454bc8ced521d8fb
zehraie <zehraie1976@gmail.com>
2024-07-13 23:23:55 +0100

    Day one and Day 2

---
+++ day01/Variables.js
@@ -0,0 +1,28 @@
+//let name;
+let name = null; // represent intentional absense, it is a bug because it says type of object
+console.log(name);
+console.log(typeof name);
+console.log('------------');
+
+//? let
+
+let age =65;  //  number integer
+let score =7.8; // number float
+console.log(age);
+console.log(score);
+console.log(typeof score);
+
+//let isEmployed ;
+let isEmployed = true;
+console.log(isEmployed);
+console.log(typeof isEmployed);
+
+//! no default value in JS, it is going to be UNDIFINED
+
+const NUMBER_OF_MONTH_IN_A_YEAR = 12;
+
+const NUMBER_OF_DAYS_IN_A_WEEK = 7;
+
+console.log(NUMBER_OF_MONTH_IN_A_YEAR);
+console.log(NUMBER_OF_DAYS_IN_A_WEEK);
+
